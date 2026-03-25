577b3186eee46d5c564865ad0c2cc3e1aa4384e9
Eduardo Tashiro <eduardogaia854@hotmail.com>
2026-03-25 03:26:35 -0300

    initial POC

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	s3Bucket := os.Getenv("S3_BUCKET")
+	fmt.Println(s3Bucket)
+
+	//POC
+	//BC
+	//"window.PogoConfig = {\"template\":\"status\",\"service\":\"pokerstars\",\"category\":\"Betting, Lotteries & Fantasy Sports\",\"outage\":false};",
+}
